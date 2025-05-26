206a7f6fd19f2b80fa1c6473ce892ab68dab1559
Erick Lasluisa <erickl1709@gmail.com>
2025-05-26 14:24:44 -0500

    Agregar decorador @Entity a la clase Deuda y reorganizar importaciones

--- src/deuda/entities/deuda.entity.ts
+++ src/deuda/entities/deuda.entity.ts
@@ -1,6 +1,13 @@
 import { Cliente } from 'src/cliente/entities/cliente.entity';
-import { PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn } from 'typeorm';
+import {
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  JoinColumn,
+  Entity,
+} from 'typeorm';
 
+@Entity('deudas')
 export class Deuda {
   @PrimaryGeneratedColumn({ name: 'id_deuda' })
   id: number;
